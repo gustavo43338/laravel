51eb1a794ea677ede5c524067c7bf2961cecbc98
gustavo43338 <gustavo.borrayo.23s@utzmg.edu.mx>
2026-05-21 11:24:56 -0600

    es este Maetra

--- app/Events/ChatMessage.php
+++ app/Events/ChatMessage.php
@@ -6,16 +6,30 @@ use Illuminate\Broadcasting\Channel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use App\Models\Mensaje;
 
 class ChatMessage implements ShouldBroadcast
 {
     use Dispatchable, SerializesModels;
 
+    public $usuario;
     public $mensaje;
+    public $id;
+    public $created_at;
 
-    public function __construct($mensaje)
+    public function __construct($usuario, $mensaje)
     {
+        $this->usuario = $usuario;
         $this->mensaje = $mensaje;
+        
+       
+        $msg = Mensaje::create([
+            'usuario' => $usuario,
+            'mensaje' => $mensaje
+        ]);
+        
+        $this->id = $msg->id;
+        $this->created_at = $msg->created_at;
     }
 
     public function broadcastOn(): array
--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,20 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Events\ChatMessage;
+use App\Models\Mensaje;
+
+Route::get('/mensajes', function () {
+    return response()->json(
+        Mensaje::orderBy('created_at', 'asc')->get()
+    );
+});
 
 Route::post('/mensaje', function (Request $request) {
 
-    event(new ChatMessage($request->mensaje));
+    event(new ChatMessage(
+        $request->usuario,
+        $request->mensaje
+    ));
 
     return response()->json([
         'ok' => true
